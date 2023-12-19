6d490904ffab4080b264cc5753dd1111e85989b1
Nabin12chand <nabin.async.chand@gmail.com>
2023-12-19 15:04:40 +0545

    added: firebase remote config

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,74 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_in_app_messaging/firebase_in_app_messaging.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_notification/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // FirebaseInAppMessaging.instance.setAutomaticDataCollectionEnabled(true);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const FirebaseInAppMessaging());
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
   }
 }
 
-class FirebaseInAppMessaging extends StatefulWidget {
-  const FirebaseInAppMessaging({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
   @override
-  State<FirebaseInAppMessaging> createState() => _FirebaseInAppMessagingState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _FirebaseInAppMessagingState extends State<FirebaseInAppMessaging> {
-  FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+class _MyHomePageState extends State<MyHomePage> {
+//  final FirebaseInAppMessaging fiam = FirebaseInAppMessaging.instance;
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
   @override
   void initState() {
     super.initState();
+
     getToken();
   }
 
   void getToken() async {
     final token = await _firebaseMessaging.getToken();
-    print("token is $token");
+    print('my token adfkdfdlf $token');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('In App messaging Demo'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                FirebaseInAppMessaging.instance.triggerEvent("show_message");
+              },
+              child: const Text('Show In-App Message'),
+            ),
+          ],
+        ),
       ),
     );
   }
